e1e3a273f4ec85f4e01325c8ca0526b7b95318d7
ardaaskinn01 <ardaaskindm@gmail.com>
2025-05-11 16:59:50 +0300

    last updates

--- src/app/projelerimiz/tavsanli/page.js
+++ src/app/projelerimiz/tavsanli/page.js
@@ -32,7 +32,7 @@ export default function Tavsanli() {
         <div className="relative min-h-screen">
             <Navbar />
 
-            <div className="absolute top-24 left-6 z-20">
+            <div className="absolute top-20 left-6 z-20">
                 <button
                     onClick={() => router.back()}
                     className="bg-orange-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-orange-600"
