ed515654f018cc3aa160a4b3067116a4a72bc73e
ardaaskinn01 <ardaaskindm@gmail.com>
2025-07-01 14:27:54 +0300

    last updates

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link href="/" className="text-2xl font-bold">
             <Image
-              src="/nrt10.png"
+              src="/nrt03.png"
               alt="Nurettin Tutak Logo"
               width={135}
               height={30}
