d0acbce4f59cdade264d2485c0d4d6b5bcefb77f
ardaaskinn01 <ardaaskindm@gmail.com>
2025-07-13 19:55:46 +0300

    last commit

--- src/app/projelerimiz/nazilli/page.js
+++ src/app/projelerimiz/nazilli/page.js
@@ -18,7 +18,7 @@ export default function Nazilli() {
       <TopContactBar />
       <Navbar />
 
-      <div className="absolute top-40 left-6 z-20">
+      <div className="absolute top-24 left-6 z-20">
         <button
           onClick={() => router.back()}
           className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-600"
